a47a60f7ded6b016f70d38824102c8d6c56d6793
boywonder3 <opeyemiisaacboywonder@gmail.com>
2024-01-24 02:13:17 +0100

    copy fix corrected

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <>
       <h1 className="text-3 md:text-2 pt-3 md:pt-1">
-        Creating a Space for Young People to{" "}
+        Creating a Space for Young People In Tech{" "}
         <span className="bg-gradient-to-r from-purple-100 via-purple-50 to-yellow-100 inline-block text-transparent bg-clip-text">
           Connect and&nbsp;Grow
         </span>{" "}
