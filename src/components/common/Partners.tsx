3a5587d8255bdf3d5ab0bc74866331772ffcd59b
Deborah Adewunmi <dbrhadewunmi@gmail.com>
2024-04-03 20:06:38 +0100

    exported repeated code for socialLinks and partners into separate files

--- src/components/common/Partners.tsx
+++ src/components/common/Partners.tsx
@@ -1,62 +1,24 @@
-import { useWindowSize } from "@uidotdev/usehooks";
+import { PartnersData } from "@/data/partnersData";
 import Image from "next/image";
-import bounce from "../../assets/commons/bounce.png";
-import greenCamp from "../../assets/commons/greenCamp.png";
-import paymentLogue from "../../assets/commons/paymentLogue.png";
-import techBabble from "../../assets/commons/techBabble.png";
-import zoropay from "../../assets/commons/zoropay.png";
 
 const Partners = () => {
-  const windowWidth = useWindowSize().width!;
+  const partners = PartnersData;
 
-  const changeValueByScreenSize = (
-    desktopValue: number,
-    mobileValue: number
-  ) => {
-    if (windowWidth > 767) {
-      return desktopValue;
-    } else return mobileValue;
-  };
   return (
     <div>
       <h3 className="text-4 md:text-2 pt-5 md:pt-1 pb-8 md:pd-4">
         Some of our recent partners
       </h3>
-      <div className="flex flex-row justify-center items-center gap-[24px] md:gap-[56px] px-[18px]">
-        <Image
-          src={zoropay}
-          alt="logo"
-          objectFit="contain"
-          className="w-[59px] h-[13px] md:w-[160px] md:h-[37px]"
-        />
-        <Image
-          src={greenCamp}
-          alt="logo"
-          objectFit="contain"
-          className="w-[29px] h-[29px] md:w-[80px] md:h-[80px]"
-        />
-        <Image
-          src={paymentLogue}
-          alt="logo"
-          objectFit="contain"
-          className="w-[77px] h-[29px] md:w-[211px] md:h-[50px]"
-        />
-        <Image
-          src={techBabble}
-          alt="logo"
-          objectFit="contain"
-          className="w-[40px] h-[30px] md:w-[112px] md:h-[50px]"
-        />
-        <Image
-          src={bounce}
-          alt="logo"
-          objectFit="contain"
-          style={{
-            width: changeValueByScreenSize(83, 30),
-            height: changeValueByScreenSize(83, 30),
-          }}
-          className="w-[30px] h-[30px] md:w-[83px] md:h-[83px]"
-        />
+      <div className="flex flex-row justify-center items-center gap-[24px] lg:gap-[56px] px-[18px]">
+        {partners.map((partner, index) => (
+          <Image
+            key={index}
+            src={partner.image}
+            alt={partner.name}
+            objectFit="contain"
+            className={partner.className}
+          />
+        ))}
       </div>
     </div>
   );
