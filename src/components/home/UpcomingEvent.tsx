6c51bc56a41b492a1c4838363f41281d7afe8fe5
Mubarak Yahaya <ymubarak247@gmail.com>
2024-04-03 21:40:51 +0100

    fature: minor fix

--- src/components/home/UpcomingEvent.tsx
+++ src/components/home/UpcomingEvent.tsx
@@ -10,7 +10,7 @@ const UpcomingEvent = () => {
   return (
     <div className="p-8 my-[4rem] bg-[#FA8A1A] w-[100%] rounded-[1.5rem] flex flex-col gap-y-[2rem] gap-x-[1rem] md:grid  md:grid-cols-2 " 
     >
-      <Image className='rounded-[1.5rem] w-[100%] ' src={upcomingEvent} alt='' style={{}} />
+      <Image className='rounded-[1.5rem] w-[100%] ' src={upcomingEvent} alt='Upcoming Event Picture' style={{}} />
       <Countdown />
       <div className='hidden md:grid md:grid-cols-2 gap-y-[2rem] gap-x-[1rem] col-span-2' >
         <Card image={card1} heading='Series B' body="We're trying to take the awkwardness out of professional" color='bg-[#FFF4E9]' headingColor="text-[#fa8a1a]" />
