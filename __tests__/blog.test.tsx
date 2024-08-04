e2cbcab66abef760a51342e16811311dda2831a3
Bewizi <toluwalase.bewaji@yahoo.com>
2024-08-04 13:57:44 +0100

    reverted to the previous commit

--- __tests__/blog.test.tsx
+++ __tests__/blog.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 import Blog from "@/pages/blog";
 
 it("should have head", () => {
