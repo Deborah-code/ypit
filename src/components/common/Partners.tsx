8346d4a0e1c592e566fbc4170a3aa29144556ccc
Deborah Adewunmi <dbrhadewunmi@gmail.com>
2024-09-16 20:29:55 +0100

    added swiper for recent partners and removed stale events page info

--- src/components/common/Partners.tsx
+++ src/components/common/Partners.tsx
@@ -1,24 +1,68 @@
 import { PartnersData } from "@/data/partnersData";
 import Image from "next/image";
+import React from "react";
+import SwiperCore from "swiper";
+import { Autoplay, Parallax } from "swiper/modules";
+import { Swiper, SwiperProps, SwiperSlide } from "swiper/react";
+
+const SwiperConfig: SwiperProps = {
+  speed: 5000,
+  autoplay: {
+    delay: 5,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+  loop: true,
+  slidesPerView: "auto",
+  watchSlidesProgress: true,
+  spaceBetween: 2,
+  grabCursor: true,
+};
+
+SwiperCore.use([Autoplay, Parallax]);
 
 const Partners = () => {
   const partners = PartnersData;
+  const swiperRef = React.useRef<SwiperCore>();
+  const [hover, onHover] = React.useState<boolean>(false);
+
+  const onInit = (Swiper: SwiperCore): void => {
+    swiperRef.current = Swiper;
+  };
+
+  const handleMouseEnter = () => {
+    onHover(true);
+    if (swiperRef.current) swiperRef.current.autoplay.start();
+  };
+  const handleMouseLeave = () => {
+    onHover(false);
+    if (swiperRef.current) swiperRef.current.autoplay.stop();
+  };
 
   return (
     <div>
       <h3 className="text-4 md:text-2 pt-5 md:pt-1 pb-8 md:pd-4">
         Some of our recent partners
       </h3>
-      <div className="flex flex-row justify-center items-center gap-[24px] lg:gap-[56px] px-[18px]">
-        {partners.map((partner, index) => (
-          <Image
-            key={index}
-            src={partner.image}
-            alt={partner.name}
-            objectFit="contain"
-            className={partner.className}
-          />
-        ))}
+      <div
+        className="highlight-inner"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        <Swiper {...SwiperConfig} onInit={onInit}>
+          {partners.map((partner, index) => (
+            <SwiperSlide style={{ marginLeft: "2rem" }}>
+              <Image
+                key={index}
+                src={partner.image}
+                alt={partner.name}
+                objectFit="contain"
+                className={partner.className}
+                style={{ maxHeight: "5rem", maxWidth: "8rem" }}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   );
