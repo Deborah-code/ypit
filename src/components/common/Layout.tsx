30adcd0386b9708a10d142d764dc09206fe38348
Chisom Ohanu <148290753+Chiblessed@users.noreply.github.com>
2024-11-14 02:54:08 +0100

    done

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,14 +1,19 @@
+import { useRouter } from 'next/router';
 import Footer from "@/components/common/Footer";
 import MobileNavBar from "@/components/common/MobileNavBar";
 import Navbar from "@/components/common/Navbar";
 
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+
 type Props = {
   children: string | JSX.Element | JSX.Element[];
 };
 
 const Layout = ({ children }: Props) => {
+  const router = useRouter();
+  const isCareerPage = router.pathname === '/career';
+
   return (
     <main
       className={`flex flex-col items-center justify-between px-[16px] py-[24px] max-w-[1340px] m-auto text-center overflow-hidden`}
@@ -16,13 +21,13 @@ const Layout = ({ children }: Props) => {
       <div className="hidden md:block w-[100%]">
         <Navbar />
       </div>
-      <div className="block  w-[100%] md:hidden">
+      <div className="block w-[100%] md:hidden">
         <MobileNavBar />
       </div>
       {children}
       <Analytics />
       <SpeedInsights />
-      <Footer />
+      {!isCareerPage && <Footer />}
     </main>
   );
 };
