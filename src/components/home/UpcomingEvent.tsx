748bdc08c394b66b660a10f9a521e26faa02de8f
Solomon Akuson <akusonsolomon15@gmail.com>
2024-11-16 00:33:55 +0100

    fix(countdown): fix countdown layout

--- src/components/home/UpcomingEvent.tsx
+++ src/components/home/UpcomingEvent.tsx
@@ -5,31 +5,6 @@ import Card from "../common/Card";
 const UpcomingEvent = () => {
   return (
     <div className="p-8 my-[4rem] bg-purple-100 w-[100%] rounded-[1.5rem] flex flex-col gap-y-[2rem] gap-x-[1rem] md:grid  md:grid-cols-2 ">
-      {/* <Image
-        className="rounded-[1.5rem] w-[100%] "
-        src={upcomingEvent}
-        alt="Upcoming Event Picture"
-        style={{}}
-      /> */}
-
-      <Card
-        videoLink={
-          "https://www.youtube.com/embed/bc8fvW2AZdY?si=c94fqI6-KL5wGykT"
-        }
-        heading="Series C"
-        body="We're trying to take the awkwardness out of professional"
-        color="bg-white"
-        headingColor="text-purple-100"
-      />
-      <Card
-        videoLink={
-          "https://www.youtube.com/embed/-zieIimbY3Y?si=PlV7PciAQD8iy7GM"
-        }
-        heading="Series D"
-        body="We're trying to take the awkwardness out of professional"
-        color="bg-white"
-        headingColor="text-purple-100"
-      />
       <div className="hidden md:grid md:grid-cols-2 gap-y-[2rem] gap-x-[1rem] col-span-2">
         <Card
           videoLink={
