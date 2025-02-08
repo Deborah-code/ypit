14c42e232c4578775551ec961a645f9d154af57a
Deborah Adewunmi <dbrhadewunmi@gmail.com>
2025-02-08 16:31:44 +0000

    added moniepoint logo

--- src/data/partnersData.tsx
+++ src/data/partnersData.tsx
@@ -5,6 +5,7 @@ import { type CSSProperties } from "react";
 import acadaWorks from "../assets/commons/acada-logo.png";
 import bounce from "../assets/commons/bounce.png";
 import greenCamp from "../assets/commons/greenCamp.png";
+import moniepoint from "../assets/commons/Moniepointlogo.png";
 import paymentLogue from "../assets/commons/paymentLogue.png";
 import productDive from "../assets/commons/productDive.png";
 import techBabble from "../assets/commons/techBabble.png";
@@ -20,6 +21,11 @@ export interface Partners {
 }
 
 export const PartnersData = [
+  {
+    name: "MoniePoint",
+    image: moniepoint,
+    style: { maxWidth: "15rem" },
+  },
   {
     name: "Acada Works",
     image: acadaWorks,
