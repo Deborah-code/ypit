ee493ccd2c5fff3ee51db46dd27c40250784f949
Tobi Balogun <98078707+tobisupreme@users.noreply.github.com>
2025-04-06 14:12:57 +0100

    chore: add prisma and DATABASE_URL to env
    - add DATABASE_URL to environment variables
    - update Prisma dependencies

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    OPEN_AI_API_KEY: process.env.OPEN_AI_API_KEY || "",
+    OPEN_AI_API_KEY: process.env.OPEN_AI_API_KEY,
+    DATABASE_URL: process.env.DATABASE_URL,
   },
 };
 
